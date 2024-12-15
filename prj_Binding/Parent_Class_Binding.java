08ca4f0e39cba806823d8a265da789ec2e2e1819
EinfacheEnte <einfacheente@gmail.com>
2024-12-15 21:01:37 +0100

    Created all needed classes for the binding demostration.

---
+++ prj_Binding/Parent_Class_Binding.java
@@ -0,0 +1,5 @@
+package prj_Binding;
+
+public class Parent_Class_Binding {
+
+}
---
+++ prj_Binding/Simple_binding.java
@@ -0,0 +1,7 @@
+package prj_Binding;
+
+public class Simple_binding {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
